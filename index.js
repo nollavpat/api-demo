5c890c554436f0ab31e0308e26b1b2ae9b23ad6e
Jairus Patrick Vallon <jairus@ubx.ph>
2020-12-05 00:42:33 +0800

    Remove code

--- index.js
+++ index.js
@@ -38,149 +38,18 @@ web3.eth.net.isListening()
     app.use(bodyParser.json()); // if you want to read more what this, https://stackoverflow.com/a/41521743
 
     app.get('/api/wallet/totalSupply', async function (req, res) {
-      try {
-        const totalSupply = await erc20Contract
-            .methods
-            .totalSupply()
-            .call({from: OWNER_ADDRESS});
-
-        res.status(200).json({
-          message: 'Successfully retrieved total supply.',
-          totalSupply: totalSupply,
-        });
-      } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-          message: 'Failed to retrieve total supply.'
-        });
-      }
     });
 
     app.get('/api/wallet/balances/:address', async function (req, res) {
-      try {
-        const address = req.params.address;
-
-        const balance = await erc20Contract
-            .methods
-            .balanceOf(address)
-            .call({from: address});
-
-        res.status(200).json({
-          message: 'Successfully retrieved balance.',
-          balance: balance,
-        });
-      } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-          message: 'Failed to retrieve balance.'
-        });
-      }
     });
 
     app.get('/api/wallet/allowances/:address', async function (req, res) {
-      try {
-        const OWNER_ADDRESS = req.params.address;
-        const senderAddress = req.query.sender
-
-        let allowance = await erc20Contract
-            .methods
-            .allowance(OWNER_ADDRESS, senderAddress)
-            .call({from: senderAddress});
-
-        res.status(200).json({
-          message: 'Successfully retrieved allowance.',
-          allowance: allowance,
-        });
-      } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-          message: 'Failed to retrieve allowance.'
-        });
-      }
     });
 
     app.post('/api/wallet/transfer', async function (req, res) {
-      try {
-        const sender = req.body.sender;
-        const recipient = req.body.recipient;
-        const amount = req.body.amount;
-        let transactionHash;
-
-        if (sender) { // sender !== null || sender !== undefined
-          // we will use transferFrom method
-          const transferFromEncodedABI = erc20Contract
-              .methods
-              .transferFrom(
-                  OWNER_ADDRESS,
-                  recipient,
-                  parseInt(amount), // parse amount to int "10" => 10
-              )
-              .encodeABI();
-          transactionHash = await buildSendTransaction(
-              sender.address,
-              sender.privateKey,
-              transferFromEncodedABI,
-          );
-        } else {
-          // we will call transfer method; called by owner
-          const transferEncodedABI = erc20Contract
-              .methods
-              .transfer(
-                  recipient,
-                  parseInt(amount), // parse amount to int "10" => 10
-              )
-              .encodeABI();
-          transactionHash = await buildSendTransaction(
-              OWNER_ADDRESS,
-              OWNER_PRIVATE_KEY,
-              transferEncodedABI,
-          );
-        }
-
-        res.status(200).json({
-          message: 'Successfully transferred funds.',
-          transactionHash: transactionHash,
-        });
-      } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-          message: 'Failed to transfer funds.'
-        });
-      }
     });
 
     app.post('/api/wallet/approve', async function (req, res) {
-      try {
-        const sender = req.body.sender;
-        const amount = req.body.amount;
-
-        const approveEncodedABI = erc20Contract
-              .methods
-              .approve(
-                  sender,
-                  parseInt(amount), // parse amount to int "10" => 10
-              )
-              .encodeABI();
-          transactionHash = await buildSendTransaction(
-              OWNER_ADDRESS,
-              OWNER_PRIVATE_KEY,
-              approveEncodedABI,
-          );
-
-        res.status(200).json({
-          message: 'Successfully approve funds.',
-        });
-      } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-          message: 'Failed to approve funds.'
-        });
-      }
     });
 
     const PORT = 8080; // the port where our api will be served
@@ -194,7 +63,7 @@ web3.eth.net.isListening()
 
     console.error(error);
 
-    process.exit(1);
+    process.exit(1); // exit the program
   });
 
 /**
